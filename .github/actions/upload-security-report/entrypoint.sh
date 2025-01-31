1b0cbc99f5df01bf24d84713bd6e2c710887e793
claudiatn <cntenorio@stemdo.io>
2025-01-31 13:02:50 +0100

    bug create engagement

--- .github/actions/upload-security-report/entrypoint.sh
+++ .github/actions/upload-security-report/entrypoint.sh
@@ -3,22 +3,33 @@
 set -e
 
 API_URL="${DEFECTDOJO_URL}/api/v2"
-HEADERS="-H 'Authorization: Token ${DEFECTDOJO_TOKEN}' -H 'accept: application/json' -H 'Content-Type: application/json'"
+HEADERS=(
+  -H "Authorization: Token ${DEFECTDOJO_TOKEN}"
+  -H "accept: application/json"
+  -H "Content-Type: application/json"
+)
 
-echo "API_URL=${DEFECTDOJO_URL}"
+echo "API_URL=${API_URL}"
 echo "Token recibido (primeros 5 caracteres): ${DEFECTDOJO_TOKEN:0:5}*****"
 
 # Buscar engagement
 echo "Buscando engagement..."
-ENGAGEMENT_ID=$(curl -s $HEADERS "${API_URL}/engagements/?product=${PRODUCT_ID}&name=${ENGAGEMENT_NAME}" | jq -r '.results[0].id')
+ENGAGEMENT_ID=$(curl -s "${API_URL}/engagements/?product=${PRODUCT_ID}&name=${ENGAGEMENT_NAME}" \
+    "${HEADERS[@]}" | jq -r '.results[0].id')
 
 # Si no existe, crear uno
 if [ "$ENGAGEMENT_ID" == "null" ] || [ -z "$ENGAGEMENT_ID" ]; then
     echo "Creando nuevo engagement..."
-    ENGAGEMENT_ID=$(curl -s -X POST $HEADERS \
-        -d "{\"name\": \"${ENGAGEMENT_NAME}\", \"product\": \"${PRODUCT_ID}\", \"status\": \"In Progress\", \"target_start\": \"$(date +%Y-%m-%d)\", \"target_end\": \"$(date -d '+1 year' +%Y-%m-%d)\"}" \
-        "${API_URL}/engagements/" | jq -r '.id')
-
+       ENGAGEMENT_RESPONSE=$(curl -s -X POST "${API_URL}/engagements/" \
+        "${HEADERS[@]}" \
+        -d "{
+              \"name\": \"${ENGAGEMENT_NAME}\",
+              \"product\": \"${PRODUCT_ID}\",
+              \"status\": \"In Progress\",
+              \"target_start\": \"$(date +%Y-%m-%d)\",
+              \"target_end\": \"$(date -d '+1 year' +%Y-%m-%d)\"
+            }")
+            
     if [ "$ENGAGEMENT_ID" == "null" ] || [ -z "$ENGAGEMENT_ID" ]; then
         echo "Error: No se pudo crear el engagement."
         exit 1
