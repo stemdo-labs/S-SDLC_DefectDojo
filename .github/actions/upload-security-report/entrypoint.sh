01de116ce80f6d970406e6f48929232a49cd8479
claudiatn <cntenorio@stemdo.io>
2025-01-31 13:12:14 +0100

    bug create engagement

--- .github/actions/upload-security-report/entrypoint.sh
+++ .github/actions/upload-security-report/entrypoint.sh
@@ -10,7 +10,7 @@ HEADERS=(
 )
 
 echo "API_URL=${API_URL}"
-echo "Token recibido (primeros 5 caracteres): ${DEFECTDOJO_TOKEN:0:5}*****"
+echo "Token recibido (primeros 5 caracteres): ${DEFECTDOJO_TOKEN}"
 
 # Buscar engagement
 echo "Buscando engagement..."
@@ -21,7 +21,9 @@ ENGAGEMENT_ID=$(curl -s "${API_URL}/engagements/?product=${PRODUCT_ID}&name=${EN
 if [ "$ENGAGEMENT_ID" == "null" ] || [ -z "$ENGAGEMENT_ID" ]; then
     echo "Creando nuevo engagement..."
        ENGAGEMENT_RESPONSE=$(curl -s -X POST "${API_URL}/engagements/" \
-        "${HEADERS[@]}" \
+        -H "Authorization: Token ${DEFECTDOJO_TOKEN}" \
+        -H "accept: application/json" \
+        -H "Content-Type: application/json" \
         -d "{
               \"name\": \"${ENGAGEMENT_NAME}\",
               \"product\": \"${PRODUCT_ID}\",
@@ -29,12 +31,11 @@ if [ "$ENGAGEMENT_ID" == "null" ] || [ -z "$ENGAGEMENT_ID" ]; then
               \"target_start\": \"$(date +%Y-%m-%d)\",
               \"target_end\": \"$(date -d '+1 year' +%Y-%m-%d)\"
             }")
-            
+
     if [ "$ENGAGEMENT_ID" == "null" ] || [ -z "$ENGAGEMENT_ID" ]; then
         echo "Error: No se pudo crear el engagement."
         exit 1
     fi
-
     echo "Nuevo engagement creado con ID: $ENGAGEMENT_ID"
 else
     echo "Engagement existente encontrado con ID: $ENGAGEMENT_ID"
